93d7f07e9d0fa465268e98be1285c32d0fa0ce28
Lekkan <lekkanm@gmail.com>
2018-04-06 17:48:45 +0530

    React Login with Local Storage

---
+++ Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Component}from 'react';
+
+export default class Home extends Component{
+    render(){
+        return(
+            <h2 className="">Home content Here</h2>
+        ); 
+    }
+}
